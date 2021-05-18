ff988d679bed53661f215a204cfdda03d8031816
Prashant <prashantchauhan1@hotmail.com>
2021-05-18 09:27:49 +0530

    V 1.0

--- dist/ng-spring-blog-frontend/main.js
+++ dist/ng-spring-blog-frontend/main.js
@@ -423,11 +423,11 @@ __webpack_require__.r(__webpack_exports__);
 
 
 var AuthService = /** @class */ (function () {
+    //private url = 'http://localhost:8080/api/auth/';
     function AuthService(httpClient, localStoraqeService) {
         this.httpClient = httpClient;
         this.localStoraqeService = localStoraqeService;
-        // private url = 'http://13.127.71.50:8080/api/auth/';
-        this.url = 'http://localhost:8080/api/auth/';
+        this.url = 'http://13.127.71.50:8080/api/auth/';
     }
     AuthService.prototype.register = function (registerPayload) {
         return this.httpClient.post(this.url + 'signup', registerPayload);
